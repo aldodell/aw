e80e02f6ee6f16210d4ea9f8f060a7dcc3508b03
Aldo Dell <aldodell@gmail.com>
2024-12-26 22:51:00 -0400

    fix

--- procesar.php
+++ procesar.php
@@ -7,23 +7,24 @@ try {
     $text = 'Text version of email';
     $html = file_get_contents("php://input");
     $html = "<!DOCTYPE html><html lang=\"en\">$html</html>";
-    $crlf = "\n";
+   
+    $crlf = "\r\n";
     $hdrs = array(
         'From' => 'aldodell@gmail.com',
         'Subject' => 'Test mime message'
     );
     
      
-    $mime = new Mail_mime(array('eol' => $crlf));   
+    $mime = new Mail_mime($crlf);   
     $mime->setTXTBody($text);
     $mime->setHTMLBody($html);
-    /*
+    
     $body = $mime->get();
     $hdrs = $mime->headers($hdrs);
     $mail =& Mail::factory('mail');
 
     $mail->send('aldodell@gmail.com', $hdrs, $body);
-    */
+    echo 'ok';
     
 } catch (Exception $e) {
     echo $e->getMessage();
