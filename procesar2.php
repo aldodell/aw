ac451961dd710bb11c18ded36b8fcda6394eac29
Aldo Dell <aldodell@gmail.com>
2024-12-27 11:20:47 -0400

    pruebas

--- procesar2.php
+++ procesar2.php
@@ -4,11 +4,11 @@ $html = "<!DOCTYPE html><html lang=\"en\">$html</html>";
 
 
 $i = 1;
-$id = "reportes/r$i";
+$id = "reportes/r$i.html";
 while (file_exists($id)) {
     $i++;
-    $id = "reportes/r$i";
+    $id = "reportes/r$i.html";
 }
 
-file_put_contents($id, $html);
+file_put_contents($id, $html, LOCK_EX);
 echo $id;
